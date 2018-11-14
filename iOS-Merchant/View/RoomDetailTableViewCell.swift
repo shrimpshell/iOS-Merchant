388ab84113181ca46585f3b1dd180efe69cd923a
lucychang <changlin0611@gmail.com>
2018-11-15 00:54:38 +0800

    新增：活動(假資料)、房間預覽頁面(假資料)，初版完成。

---
+++ iOS-Merchant/View/RoomDetailTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  RoomDetailTableViewCell.swift
+//  Employee
+//
+//  Created by Lucy on 2018/11/8.
+//  Copyright © 2018 Lucy. All rights reserved.
+//
+
+import UIKit
+
+class RoomDetailTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
