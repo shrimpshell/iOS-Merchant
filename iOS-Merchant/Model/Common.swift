2aae8406dda7bb7fcf7c44942602d849d903bd74
Josh Hsieh <halu_9@hotmail.com>
2018-12-07 12:33:58 +0800

    123

--- iOS-Merchant/Model/Common.swift
+++ iOS-Merchant/Model/Common.swift
@@ -29,12 +29,12 @@ typealias DoneHandler = (_ result: Any?, _ error: Error?) -> Void
 struct Common {
     
     // Server
-    //static let SERVER_URL: String = "http://192.168.50.124:8080/ShellService"
-    static let SERVER_URL: String = "http://10.1.1.3:8080/ShellService"
+    static let SERVER_URL: String = "http://192.168.50.124:8080/ShellService"
+    //static let SERVER_URL: String = "http://10.1.1.3:8080/ShellService"
     
     // Websocket
-    //let SOCKET_URL: String = "ws://192.168.50.124:8080/ShellService/WsServer/"
-    let SOCKET_URL: String = "ws://10.1.1.3:8080/ShellService/WsServer/"
+    let SOCKET_URL: String = "ws://192.168.50.124:8080/ShellService/WsServer/"
+    //let SOCKET_URL: String = "ws://10.1.1.3:8080/ShellService/WsServer/"
 
     
     
