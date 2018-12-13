2f42b39d6f352ba994e46470ddf8348f079d2264
Josh Hsieh <halu_9@hotmail.com>
2018-12-13 14:58:32 +0800

    IP

--- iOS-Merchant/Model/Common.swift
+++ iOS-Merchant/Model/Common.swift
@@ -30,11 +30,11 @@ struct Common {
     
     // Server
     static let SERVER_URL: String = "http://192.168.50.124:8080/ShellService"
-    //static let SERVER_URL: String = "http://192.168.50.105:8080/ShellService"
+    //static let SERVER_URL: String = "http://10.1.1.3:8080/ShellService"
     
     // Websocket
     let SOCKET_URL: String = "ws://192.168.50.124:8080/ShellService/WsServer/"
-    //let SOCKET_URL: String = "ws://192.168.50.105:8080/ShellService/WsServer/"
+    //let SOCKET_URL: String = "ws://10.1.1.3:8080/ShellService/WsServer/"
 
     
     
